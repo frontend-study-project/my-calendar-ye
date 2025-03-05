833109ef241196b7feb3699d18560fae87ce048c
yennnny <jye104@nate.com>
2025-03-05 15:50:12 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  plugins: [react()],
+});
